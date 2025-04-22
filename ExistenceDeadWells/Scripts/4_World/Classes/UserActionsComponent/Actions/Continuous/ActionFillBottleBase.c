d964353e991fdb600af2465d96e931e1f9fa3eb6
CostaGeoDev <CostaGeoDev@outlook.com>
2025-04-22 22:34:22 +0100

    Update ActionFillBottleBase.c

--- ExistenceDeadWells/Scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/ActionFillBottleBase.c
+++ ExistenceDeadWells/Scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/ActionFillBottleBase.c
@@ -36,4 +36,21 @@ modded class ActionFillBottleBase : ActionContinuousBase
             action_data.m_Player.SetPreventUseWell(false);
         }
     }
+
+    // If enableWellTransferCholera is true, then the player has a chance to get cholera when drinking from the well.
+    override void OnEndServer(ActionData action_data)
+    {
+        Well well = Well.Cast(action_data.m_Target.GetObject());
+
+        if (well)
+        {            
+            if (well.GetDeadWellConfig().enableWellTransferCholera && well.GetDeadWellConfig().choleraChance > 0)
+            {
+                if (Math.RandomIntInclusive(0, 100) <= well.GetDeadWellConfig().choleraChance)
+                {
+                    action_data.m_MainItem.InsertAgent(eAgents.CHOLERA, 1);
+                }
+            }
+        }
+    }
 }
